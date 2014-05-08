b4a59c5588343a2a9d132de2bfbfb5f7f14634b1
Michael Klopf <klopf.michael@gmail.com>
2014-05-08 11:44:01 +0200

    performantere for schleifen

--- js/bsp_02.js
+++ js/bsp_02.js
@@ -14,10 +14,26 @@ for (var i = 0; i < myArray.length; i++ ) {
 
 // der klassische for schleifen Durchlauf ist unperformant und ein suboptimales Pattern
 
+// ----------------
 // Besseres Pattern
-var i;
-var max;
+// ----------------
+var i; // Iterator
+var max; // Arraylaenge
 
+// Das setzen der Werte geschieht initial einmal,
+// die Abfrage auf die Laenge bei jedem Durchlauf,
+// als auch die Inkrementation
 for (i = 0, max = myArray.length; i < max; i++) {
     console.log(myArray[i]);
 }
+
+// ----------------
+// Noch besseres Pattern
+// ----------------
+var i; // Iterator
+
+// noch performanter
+for (i=myArray.length;i--;) {
+    console.log(myArray[i]);
+}
+
