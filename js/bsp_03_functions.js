e7d79cf4b3c449c5f1ddc8853b6a8f30bb7d2109
Michael Klopf <klopf.michael@gmail.com>
2014-05-08 13:31:11 +0200

    functions basics

--- js/bsp_03_functions.js
+++ js/bsp_03_functions.js
@@ -1,2 +1,26 @@
 // Funktionen
 console.log("bsp_03_functions.js");
+
+// Block scope
+// Private variables
+// Closures
+
+// ----------------
+// basics
+// ----------------
+// Beim Parsen des JS Scripts wird der Code zweimal durchlaufen. Erst die Suche nach Funktionen, dann die Ausfuehrung.
+
+// Funktion kann aufgrund dieser Tatsache jederzeit gerufen werden
+logSomething();
+
+function logSomething() {
+    console.log("something");
+}
+
+// Dies kann wie folgt unterbunden werden
+var logSomethingElse = function() {
+    console.log("somethingElse");
+}; // man beachte das Semikolon
+
+logSomethingElse();
+
