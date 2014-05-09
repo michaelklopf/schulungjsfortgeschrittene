a8f923422a9b0b6eea5b77817877d40c53f64a9c
Michael Klopf <klopf.michael@gmail.com>
2014-05-09 09:27:42 +0200

    More Method chaining.

--- js/bsp_07_chaining.js
+++ js/bsp_07_chaining.js
@@ -10,8 +10,30 @@ Auto.prototype = {
   constructor: Auto,
   add : function() {
     this.personen += 1;
+
+    return this; // (1) zuerst hinzugefuegt fuer chaining
   },
   alertAnzahl : function() {
-    alert("Anzahl " + this.personen);
+    console.log("Anzahl " + this.personen);
+
+    return this; // (2) als zweites hinzugefuegt fuer chaining
   }
 };
+
+var porsche = new Auto();
+
+porsche.alertAnzahl();
+porsche.add();
+porsche.alertAnzahl();
+
+// dies sieht nicht gut aus, dreimal porsche
+(porsche.add()).alertAnzahl();  // fuehrt zu Fehler wenn (1) auskommentiert
+// Klammerausdruck liefert this zurueck was auf unseren Porsche verweist
+
+// Laesst sich vereinfachen zu
+porsche.add().alertAnzahl();
+
+(porsche.alertAnzahl()).add(); // fuehrt zu Fehler wenn (2) auskommentiert
+
+// Jetzt geht alles
+porsche.alertAnzahl().add().alertAnzahl().add().add().alertAnzahl();
