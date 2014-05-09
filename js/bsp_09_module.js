d37aa004c28c6ce0c55ca5874f729b105a2af7b2
Michael Klopf <klopf.michael@gmail.com>
2014-05-09 10:11:16 +0200

    Module pattern complete.

--- js/bsp_09_module.js
+++ js/bsp_09_module.js
@@ -1,2 +1,28 @@
 // Module Pattern
 console.log("bsp_09_module.js");
+
+var myModule = function() {
+  'use strict';
+  var one = 1;
+  var two = 2;
+
+  var privateFunction = function() {}; // besser _privateFunction
+
+  var publicFunction = function() {
+    return one;
+  };
+
+  return {
+    getOne : publicFunction,
+    //besser:
+    getTwo : function() {
+      return two;
+    }
+  }
+}();
+
+var one = myModule.getOne();
+var two = myModule.getTwo();
+
+console.log(one);
+console.log(two);
