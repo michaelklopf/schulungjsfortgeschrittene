7999c978f9ef1180d40fa0325b5352faf9f45170
Michael Klopf <klopf.michael@gmail.com>
2014-05-09 09:38:30 +0200

    Namespace pattern.

---
+++ js/bsp_08_namespace_pattern.js
@@ -0,0 +1,34 @@
+// Namespace Pattern
+console.log("bsp_08_namespace_pattern.js");
+
+// Antipattern - so macht man es nicht
+function execute() {
+
+}
+execute();
+var values = {a:1, b:2};
+
+// kombinierbar mit Singleton (immer nur genau ein Modul erzeugen)
+
+// Hier das bessere Pattern:
+var App = {}; // unsicher denn es koennte App zweimal vorkommen
+
+App.Fahrzeug = function() {};
+App.Auto = function() {};
+
+App.modul = {};
+
+// deswegen pruefen auf
+if (typeof App === "undefined") {
+  var App = {};
+}
+
+// Oder Anonymer Namensraum
+(function() {
+  var App = {};
+
+  App.Fahrzeug = function() {};
+  App.Auto = function() {};
+
+  App.modul = {};
+})();
